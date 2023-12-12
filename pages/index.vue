08acff17191417b637d7eb0ce4454e98525a6445
Ruan Aragão <ruanaragao2@gmail.com>
2023-12-12 01:19:01 -0300

    feat: add main index page

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="desktop overflow-hidden">
+    <LauncherGrid />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
+
+<style>
+html {
+  overflow: hidden;
+}
+
+* {
+  box-sizing: border-box;
+  user-select: none;
+}
+
+.desktop {
+  background: url('/bg.png') no-repeat center center fixed;
+  background-size: cover;
+}
+</style>
